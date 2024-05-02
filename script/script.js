baa87099d35833c631b79da83ca2773800c0a3dc
Nikola Gedovic <nikola.gedovic@gmail.com>
2024-05-02 22:09:16 +0200

    making hamburger menu for mobile

--- script/script.js
+++ script/script.js
@@ -1,40 +1,52 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let interval = null;
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let interval = null;
 
-  function changeTextLetter(element) {
-    let iteration = 0;
+function changeTextLetter(element) {
+  let iteration = 0;
 
-    // get initial text
-    const initText = element.innerText;
+  // get initial text
+  const initText = element.innerText;
 
-    // clear previous interval
-    clearInterval(interval);
+  // clear previous interval
+  clearInterval(interval);
 
-    // set interval to change text letter by letter 
-    interval = setInterval(() => {
-      element.innerText = initText
-        .split("")
-        .map((letter, index) => {
-          if (index < iteration) {
-            return element.dataset.textValue[index];
-          }
+  // set interval to change text letter by letter 
+  interval = setInterval(() => {
+    element.innerText = initText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return element.dataset.textValue[index];
+        }
 
-          return letters[Math.floor(Math.random() * 26)];
-        })
-        .join("");
-      if (iteration >= element.dataset.textValue.length) {
-        clearInterval(interval);
-      }
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+    if (iteration >= element.dataset.textValue.length) {
+      clearInterval(interval);
+    }
 
-      // increase iteration
-      iteration += 1 / 10;
-    }, 50);
-  }
+    // increase iteration
+    iteration += 1 / 10;
+  }, 50);
+}
 
-  const animTexts = document.querySelectorAll(".animated-text");
+const animTexts = document.querySelectorAll(".animated-text");
 
-  animTexts.forEach((element) => {
-    changeTextLetter(element);
-  });
+animTexts.forEach((element) => {
+  changeTextLetter(element);
 });
+
+// Hamburger Menu Script
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navbar-list-container");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
