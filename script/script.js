444b4d64591ba22e6704bbd6531225f6b5e1d403
Nikola Gedovic <nikola.gedovic@gmail.com>
2024-05-10 17:24:24 +0200

    making play button for main page

--- script/script.js
+++ script/script.js
@@ -1,42 +1,3 @@
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let interval = null;
-
-function changeTextLetter(element) {
-  let iteration = 0;
-
-  // get initial text
-  const initText = element.innerText;
-
-  // clear previous interval
-  clearInterval(interval);
-
-  // set interval to change text letter by letter 
-  interval = setInterval(() => {
-    element.innerText = initText
-      .split("")
-      .map((letter, index) => {
-        if (index < iteration) {
-          return element.dataset.textValue[index];
-        }
-
-        return letters[Math.floor(Math.random() * 26)];
-      })
-      .join("");
-    if (iteration >= element.dataset.textValue.length) {
-      clearInterval(interval);
-    }
-
-    // increase iteration
-    iteration += 1 / 10;
-  }, 50);
-}
-
-const animTexts = document.querySelectorAll(".animated-text");
-
-animTexts.forEach((element) => {
-  changeTextLetter(element);
-});
-
 // // Hamburger Menu Script
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navbar-list-container");
@@ -63,4 +24,3 @@ document.querySelectorAll(".nav-links").forEach(n => n.addEventListener("click",
   // Reset the main content position when a nav link is clicked
   contentPlaceholder.style.marginTop = '0';
 }));
-
